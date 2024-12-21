b3ceac7f420be64cf0ea10c9fa9f7c3801e3d5d6
Hattory2110 <csomorbalint6@gmail.com>
2024-12-21 21:33:00 +0100

    Added basic entity classes.

---
+++ src/main/java/hu/unideb/inf/weblib/data/entities/BookEntity.java
@@ -0,0 +1,96 @@
+package hu.unideb.inf.weblib.data.entities;
+import jakarta.persistence.*;
+import java.util.Objects;
+import java.util.Date;
+
+@Entity
+@Table(name = "book")
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long lsz; //Leltári szám
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "genre", nullable = false)
+    private String genre;
+    @Column(name = "writer", nullable = false)
+    private String writer;
+    @Column(name = "pubisher", nullable = false)
+    private String pubisher;
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    public BookEntity() {
+    }
+
+    public BookEntity(long lsz, String title, String genre, String writer, String pubisher, Date date) {
+        this.lsz = lsz;
+        this.title = title;
+        this.genre = genre;
+        this.writer = writer;
+        this.pubisher = pubisher;
+        this.date = date;
+    }
+
+    public long getLsz() {
+        return lsz;
+    }
+
+    public void setLsz(long lsz) {
+        this.lsz = lsz;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getPubisher() {
+        return pubisher;
+    }
+
+    public void setPubisher(String pubisher) {
+        this.pubisher = pubisher;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookEntity that = (BookEntity) o;
+        return lsz == that.lsz && date == that.date && Objects.equals(title, that.title) && Objects.equals(genre, that.genre) && Objects.equals(writer, that.writer) && Objects.equals(pubisher, that.pubisher);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lsz, title, genre, writer, pubisher, date);
+    }
+}
---
+++ src/main/java/hu/unideb/inf/weblib/data/entities/BorrowEntity.java
@@ -0,0 +1,85 @@
+package hu.unideb.inf.weblib.data.entities;
+
+import jakarta.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+@Entity
+@Table(name = "borrow")
+public class BorrowEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "Osz")
+    private long osz;
+    @Column(name = "Lsz")
+    private long lsz;
+    @Column(name = "borrowdate")
+    private Date borrowdate;
+    @Column(name = "duedate")
+    private Date duedate;
+
+    public BorrowEntity() {
+    }
+
+    public BorrowEntity(long id, long osz, long lsz, Date borrowdate, Date duedate) {
+        this.id = id;
+        this.osz = osz;
+        this.lsz = lsz;
+        this.borrowdate = borrowdate;
+        this.duedate = duedate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getOsz() {
+        return osz;
+    }
+
+    public void setOsz(long osz) {
+        this.osz = osz;
+    }
+
+    public long getLsz() {
+        return lsz;
+    }
+
+    public void setLsz(long lsz) {
+        this.lsz = lsz;
+    }
+
+    public Date getBorrowdate() {
+        return borrowdate;
+    }
+
+    public void setBorrowdate(Date borrowdate) {
+        this.borrowdate = borrowdate;
+    }
+
+    public Date getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(Date duedate) {
+        this.duedate = duedate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BorrowEntity that = (BorrowEntity) o;
+        return id == that.id && osz == that.osz && lsz == that.lsz && Objects.equals(borrowdate, that.borrowdate) && Objects.equals(duedate, that.duedate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, osz, lsz, borrowdate, duedate);
+    }
+}
