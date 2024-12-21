5aa80b583f3b2cf257677dd81666ddf7a3c1fe69
Hattory2110 <csomorbalint6@gmail.com>
2024-12-21 21:00:35 +0100

    Initial commit, with added necessary dependencies for whole web application

---
+++ src/main/java/hu/unideb/inf/weblib/WebLibraryApplication.java
@@ -0,0 +1,21 @@
+package hu.unideb.inf.weblib;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class WebLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLibraryApplication.class, args);
+    }
+}
---
+++ src/test/java/hu/unideb/inf/weblib/WebLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.weblib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
