e941847f11883f6f6eb33f0499dcc3a7490bd27b
Hattory2110 <csomorbalint6@gmail.com>
2024-12-22 01:28:39 +0100

    Added CRUD methodes for Book entity.

--- src/main/java/hu/unideb/inf/weblib/controller/BookController.java
+++ src/main/java/hu/unideb/inf/weblib/controller/BookController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class BookController {
@@ -17,6 +20,27 @@ public class BookController {
     @GetMapping("/books")
     public String books(){
         return "No books available!";
+    }
+
+    @GetMapping("/allbook")
+    public List<BookEntity> getAllBook(){
+        return repository.findAll();
+    }
+
+
+    @GetMapping("/book/{writer}")
+    public List<BookEntity> getBookByWriter(@PathVariable String meret){
+        List<BookEntity> meretValogatott = new ArrayList<>();
+        meretValogatott = repository.findAll()
+                .stream()
+                .filter(x -> x.getWriter().equals(meret))
+                .toList();
+        return meretValogatott;
+    }
+    // /ruha?meret=M
+    @GetMapping("/ruha")
+    public List<BookEntity> getBookByWriterDb(@RequestParam String writer){
+        return repository.findAllByWriter(writer);
     }
 
     @PostMapping("/savebook")
